84c01f7e752ffd2c559a11a25bd12884cdca32b2
Ankush Garg <1998anky@gmail.com>
2017-06-22 13:49:24 +0530

    Minor Fixes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
+function day(x)
+  {
+  if (x == 1)
+    return 'Monday';
+  else if (x == 2)
+    return 'Tuesday';
+  else if (x==3)
+    return 'Wednesday';
+  else if (x==4)
+    return 'Thursday';
+  else if (x==5)
+    return 'Friday';
+  else if (x==6)
+    return 'Saturday';
+  else if (x==7)
+    return 'Sunday';
+  }
+$('#ttbox').hide();
 function run()
   {
+  //Form Validation Pending
   var rows = $('#weekdays').val();
   var columns = $('#periods').val();
   var table = '<table class="table table-striped table-hover table-bordered">';
-  //table += '<tr><th>A</th><th>B</th><th>C</th></tr>';
+  table += '<tr class="info"><th>Days/Periods</th>';
+  for(var i=1;i<=columns;i++)
+    {
+    table += '<th> Period '+i+'</th>';
+    }
+  table += '</tr>';
   for(var i=1; i<=rows; i++)
     {
-    table += '<tr><td class="success">Day ' + i + '</td>';
+    table += '<tr><td class="info"><strong>' + day(i) + '</strong></td>';
     for(var j=1; j<=columns; j++)
       {
       table += '<td>yo</td>';
@@ -14,6 +38,5 @@ function run()
     table += '</tr>';
     }
   table += '</table>'
-  // $('#main-form').remove();
   $('#main-table').html(table);
   }
